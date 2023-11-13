98e8da5f6473bddf230c1b379f9cee6283187036
Mahdi Tasha <imwhdiiii@gmail.com>
2023-11-13 16:26:47 +0330

    added tailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,20 @@
-import type { Config } from 'tailwindcss'
+// Codes by mahdi tasha
+// Importing config type of tailwindCSS
+import type { Config } from 'tailwindcss';
 
+// Defining configs of tailwindCSS
 const config: Config = {
-  content: [],
+  content: [
+      './app/*.{jsx,tsx,ts,js}',
+      './app/**/*.{jsx,tsx,ts,js}',
+      './component/*.{jsx,tsx,ts,js}',
+      './component/**/*.{jsx,tsx,ts,js}',
+      './chunk/*.{jsx,tsx,ts,js}',
+      './chunk/**/*.{jsx,tsx,ts,js}',
+  ],
   theme: {},
   plugins: [],
 }
-export default config
+
+// Exporting configs of tailwindCSS
+export default config;
